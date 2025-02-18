d98d6f59ff92e8b6b23e23c3bb61dbb4157ddfc5
nxcc <>
2025-02-18 07:33:34 +0100

    fix decrypt of  sops:formats.Binary

--- internal/source/decrypt.go
+++ internal/source/decrypt.go
@@ -14,17 +14,21 @@ import (
 
 // decryptSopsEncryptedData tries to transparently decrypt SOPS encrypted data
 func decryptSopsEncryptedData(filenamne string, src io.Reader) (io.ReadCloser, error) {
-	format, found := knownSopsFormats[filepath.Ext(filenamne)]
-	if !found {
-		return io.NopCloser(src), nil
-	}
 	buffer := bytes.Buffer{}
 	if _, err := io.Copy(&buffer, src); err != nil {
 		return nil, fmt.Errorf("decryptSopsEncryptedData: copy: %w", err)
 	}
-	cleartext, err := decrypt.DataWithFormat(buffer.Bytes(), format)
+	format, found := knownSopsFormats[filepath.Ext(filenamne)]
+	if !found {
+		format = formats.Binary
+	}
+	contents := buffer.Bytes()
+	if !looksLikeSopsEncrypted(contents, format) {
+		return io.NopCloser(bytes.NewReader(contents)), nil
+	}
+	cleartext, err := decrypt.DataWithFormat(contents, format)
 	if err != nil && errors.Is(err, sops.MetadataNotFound) {
-		return io.NopCloser(bytes.NewReader(buffer.Bytes())), nil
+		return io.NopCloser(bytes.NewReader(contents)), nil
 	}
 	if err != nil {
 		return nil, fmt.Errorf("decryptSopsEncryptedData: DataWithFormat: %w", err)
@@ -38,4 +42,24 @@ var knownSopsFormats = map[string]formats.Format{
 	".sops": formats.Binary,
 	".yaml": formats.Yaml,
 	".yml":  formats.Yaml,
+}
+
+var probeStringsJsonOrYamlOrBinary = []string{
+	"sops", "version", "unencrypted_suffix", "lastmodified", "mac",
+}
+
+var probeStringsEnv = []string{
+	"sops_version", "sops_unencrypted_suffix", "sops_lastmodified", "sops_mac",
+}
+
+func looksLikeSopsEncrypted(contents []byte, format formats.Format) bool {
+	probes := probeStringsJsonOrYamlOrBinary
+	if format == formats.Dotenv {
+		probes = probeStringsEnv
+	}
+	allMatched := true
+	for _, probe := range probes {
+		allMatched = allMatched && bytes.Contains(contents, []byte(probe))
+	}
+	return allMatched
 }
